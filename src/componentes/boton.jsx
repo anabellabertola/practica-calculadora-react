e3f77545860ce3b20e9b5d9712324aea6e12cf9d
anabellabertola <137234052+anabellabertola@users.noreply.github.com>
2024-06-24 18:59:01 +0200

    Actualizar boton.jsx

--- src/componentes/boton.jsx
+++ src/componentes/boton.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 function Boton (props) {
+
+
+const esOperador () => {
+
   return (
-div className={`boton-contenedor $(esOperador(props.children) ? 'operador' : ''}`.trimEnd();
+div className={`boton-contenedor $(esOperador(props.children) ? 'operador' : ''}`.trimEnd()};
     
+
+export default Boton;
